a33ac41578e620a7db19c11e29d71a7971a3c259
Florencia Mazzola <87549062+FlorenciaMazzola@users.noreply.github.com>
2022-10-27 13:56:27 -0300

    Se agrega carpeta assets con estilo y script

---
+++ assets/script/script.js
@@ -0,0 +1,25 @@
+function calcTotal () {
+   cantidad = document.getElementById('cantInput').value;
+    categoria = document.getElementById('catSelect').value;
+    cantidad = parseInt(cantidad);
+    categoria = parseInt(categoria);
+    valor =200;
+    total = 0;
+    if (categoria ==1) {
+        total = cantidad * (valor -(valor * 0.8));
+    } else if (categoria ==2){
+        total = cantidad * (valor - (valor * 0.5));   
+        
+    } else if (categoria ==3){
+        total = cantidad * (valor -(valor * 0.15));   
+       
+    } else{
+        total = cantidad * valor;
+    }
+
+    document.getElementById('cantTotal').innerHTML = "Total a Pagar: $ "+total;   
+  }
+
+function borrarTotal(){
+    document.getElementById('cantTotal').innerHTML = "Total a Pagar: $ ";
+}
